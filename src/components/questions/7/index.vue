e1f11ff41b81cb2ef618177006a9e6efd1785924
Victor <smolyakov.92@bk.ru>
2021-03-11 18:41:12 +0500

    Task 7

--- src/components/questions/7/index.vue
+++ src/components/questions/7/index.vue
@@ -1,21 +1,38 @@
 <template>
     <div>
-      <input :value="value" class="phone" placeholder="phone" />
-      <span v-if="error">Не правильно введен номер телефона</span>
+      <input v-model.trim="phone" class="phone" placeholder="phone" />
+      <span v-if="error">Неправильно введен номер телефона</span>
     </div>
-
 </template>
 
 <script>
 // TODO: реализовать валидацию входных данных свойства и валидацию input - тип телефон, вида +7(XXX)-XXX-XX-XX
+//TODO: Сделал неправильно, т.к. не работал валидатор на prop value
 export default {
   name: "PhoneValidation",
   props: {
-    value: {},
+    value: {
+      type: String,
+      default: '',
+      required: true,
+    },
   },
   data() {
     return {
       error: Boolean
+    }
+  },
+  computed: {
+    phone: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        const predicate = value.match(/^\+7\(\d{3}\)-\d{3}-\d{2}-\d{2}$/) !== null;
+
+        this.value = predicate ? value : this.value
+        this.error = !predicate
+      }
     }
   },
 }
