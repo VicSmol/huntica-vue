139df423cef181d97a2dccb959d8947860527884
Victor <smolyakov.92@bk.ru>
2021-03-11 19:10:13 +0500

    Task 12

---
+++ src/components/questions/12/UserComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="user">
+    Username: {{ user.username }}<br>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "UserComponent",
+    props: {
+      value: {}
+    },
+    computed: {
+      user: {
+        get() {
+          return this.value
+        },
+        set(value) {
+          this.value = value
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/questions/12/index.vue
+++ src/components/questions/12/index.vue
@@ -1,18 +1,19 @@
 <template>
   <!-- TODO: сделать компонент безопасным для использования и написать unit тесты с помощью Jest -->
+  <!--  TODO: суть задания понимаю, но в силу малой практики с jest (тесты писал в проекте с nestjs), нужно отдельно вникать как что подключается и пишется-->
   <div>
-    <div v-if="user">
-      Username: {{ user.username }}<br>
-    </div>
+    <user-component v-model="user"></user-component>
     <button @click="fetchUser">fetch user</button>
   </div>
 </template>
 
 <script>
 import {asyncFetchUser} from "@/components/questions/12/helpers/asyncFetchUser";
+import UserComponent from "./UserComponent";
 
 export default {
   name: "ComponentForTest",
+  components: {UserComponent},
   data() {
     return {
       user: null,
